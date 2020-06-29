c3c8706540e267411386cb3b977661e6271121e4
root <root@132ab5cc5f64>
2020-06-29 20:27:45 +0000

    made awsome shapes :)

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -1,54 +1,73 @@
 package _02_nested_loops._1_snowflake;
 
+import java.awt.AWTException;
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class Snowflake {
-	
+
 	public static void main(String[] args) {
+		Robot rob = new Robot(300, 300);
 
 		// #1. Make a new Robot
 
-
 		// #2. Set your robot’s position to x=300 and y=300
 
-
+		rob.penDown();
 		// #3. Put the robot's pen down
 
-
 		// #4. Set the robot’s speed to 5
+		rob.setSpeed(20);
+int hmm = 20;
 
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
+		for (int i = 0; i < 40; i++) {
 
-			
 			// #6. Set the pen color to black
 
+			if (i % 2 == 0) {
+				rob.setPenColor(Color.blue);
+				rob.move(50);
+				rob.turn(60);
+			} if (i % 3 == 0) {
+				rob.setPenColor(Color.red);
+				rob.move(50);
+				rob.turn(60);
+			}
+			if (i%6 ==0) {
+				rob.move(50);
+			}
 			
+				
+			
+		}
+		
 			// #7. move the robot by 50
-
 			
 			// #8. turn the robot to the right by 60
 			
-			
+		
 			// #12. set the pen color to blue for even numbers of i
 
-			
 			// #13. set the pen color to red for all odd numbers of i
 			
-			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
+
 		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
-			// #14. set the pen color to black
-				
+		// #10. turn the robot to the left by 60
+		//for (int j = 0; j < 6; j++) {
+
+			//rob.turn(60);
+
+			// #11. move the robot by 50 (play the start button)
+			//rob.move(50);
+		//}
+
+		// #14. set the pen color to black
+
 		
 	}
 
