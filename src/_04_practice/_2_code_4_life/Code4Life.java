b6e9cf12d7855033fa86edebf55afaa1582a4246
root <root@d428aa78611f>
2020-07-02 20:09:36 +0000

    Almost there

--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -6,20 +6,33 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+	String hours =JOptionPane.showInputDialog("How many hours did you spend coding this week?");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+int code = Integer.parseInt(hours);
+if(code<3) {
+	JOptionPane.showMessageDialog(null, "Stop watching youtube and code");
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+if (code>=3&&code<6) {
+	JOptionPane.showMessageDialog(null, "Your a code ninja");
+	
+}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
 	// NOTE: You may have to get batman.wav from league-sounds on github: https://github.com/jointheleague/league-sounds
-
+if (code>5) {
+	playBatmanTheme();
+	JOptionPane.showMessageDialog(null, "playing batman theme");
+}
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
